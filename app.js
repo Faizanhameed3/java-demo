69e4ae39413963337eef791e70d0f7b65d0e8614
Faizanhameed3 <Faizzyhere@example.com>
2024-02-26 03:13:33 +0500

    add var + alret

---
+++ app.js
@@ -0,0 +1,16 @@
+// var thismessage="this is a new message for this"
+
+// alert(thismessage)
+
+// var percantage=100;
+// var total=100;
+// var average=40;
+// alert(percantage/total*average
+// +"%"
+// )
+var obtain =40;
+var total =50;
+var percantag=(obtain*total/100)+"%"
+alert(percantag);
+
+
