d8e5a1e3accd319ad0987d3e4d50ef32af198af8
Faizanhameed3 <Faizzyhere@example.com>
2024-02-27 03:01:48 +0500

    add

--- app.js
+++ app.js
@@ -8,9 +8,33 @@
 // alert(percantage/total*average
 // +"%"
 // )
-var obtain =40;
-var total =50;
-var percantag=(obtain*total/100)+"%"
-alert(percantag);
+// var obtain =40;
+// var total =50;
+// var percantag=(obtain*total/100)+"%"
+// alert(percantag);
+
+// var  birth=2024
+// var  year=1993
+// alert(birth - year) 
+// var day=1
+// var hours=24
+// var minut=60
+// var second=60
+// alert(1*24*60*60)
+
+var num1=40
+var num2=10
+var sum=num1+num2
+var sub=num1-num2
+var multiply=num1*num2
+var divid=num1/num2
+var mudular=num1 % num2
+alert(sum)
+alert(sub)
+alert(multiply)
+alert(divid)
+alert(mudular)
 
 
+
+ 
